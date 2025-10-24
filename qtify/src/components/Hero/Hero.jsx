5c3c8afffc0b670746b2cfef8922c8a082ea22b3
HM-Henil Modi <imhenilmodi@gmail.com>
2025-10-24 22:03:56 +0530

    Update Hero.jsx

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,14 +1,19 @@
+// src/components/Hero/Hero.jsx
+import React from "react";
 import styles from "./hero.module.css";
 import heroImage from "../../assets/hero.svg";
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div>
-        <h1> 100 Thousand Songs, ad - free </h1>
-        <h1> Over thousands podcast episodes</h1>
+      <div className={styles.textContainer}>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h2>Over thousands podcast episodes</h2>
       </div>
-      <img src={heroImage} alt="Hero_Image" />
+
+      <div className={styles.imageContainer}>
+        <img src={heroImage} alt="headphones" />
+      </div>
     </div>
   );
 };
